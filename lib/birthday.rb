2d562caa309fcac8d7cebb2f4c5bca10847b3455
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-20 16:33:47 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,7 +15,7 @@ def age_appropriate_birthday(birthday_kids)
     if age > 12
       puts "You are too old for this."
     else
-      happy_birthday(birthday_kids)
-    end
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+          end
   end
 end
