a7376e093433409915c6c16e32607a6bd3457986
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-20 16:34:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -18,4 +18,5 @@ def age_appropriate_birthday(birthday_kids)
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
           end
   end
+  age_appropriate_birthday(birthday_kids)
 end
